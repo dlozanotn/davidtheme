88f58d56743e0ac9737fd7081d4049b1031a507e
dlozano <dlozanotn@gmail.com>
2022-11-04 21:11:34 -0600

    new changes

--- functions.php
+++ functions.php
@@ -1,5 +1,15 @@
 <?php 
 
+function david_theme_support () {
+
+    add_theme_support( 'title_tag' );
+
+}
+
+add_action( 'after_setup_theme', 'david_theme_support' );
+
+ 
+
 function david_register_styles () {
 
     wp_enqueue_style('david-styles', get_template_directory_uri() . "/style.css", array('david-bootstrap'), '1.0', 'all');
@@ -7,8 +17,19 @@ function david_register_styles () {
     wp_enqueue_style('david-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
 
-add_action( 'wp_enqueue_scripts', 'david_register_styles' )
+add_action( 'wp_enqueue_scripts', 'david_register_styles' );
 
+
+function david_register_scripts () {
+
+    wp('david-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),  '3.4.1', true);
+    wp_enqueue_script('david-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_script('david-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '4.4.1', true);
+    wp_enqueue_script('david-jquery', get_template_directory_uri() . "/assets/js/main.js", array(),  '1.0', true);
+}
+
+
+add_action('wp_enqueue_scripts', 'david_register_scripts' );
 
 
 ?>
