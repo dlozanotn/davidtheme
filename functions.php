f2ec6db320b7221f1656db6567bd472754d4773c
dlozano <dlozanotn@gmail.com>
2022-11-08 22:22:23 -0600

    finish nav, and logo

--- functions.php
+++ functions.php
@@ -2,8 +2,9 @@
 
 function david_theme_support () {
 
-    add_theme_support( 'title_tag' );
-
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'custom-logo');
+    add_theme_support( 'post-thumbnails' );
 }
 
 add_action( 'after_setup_theme', 'david_theme_support' );
